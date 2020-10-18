eb7354f1f86cf3d4220d8d47828c06ca72280624
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-10-18 19:40:40 +0300

    Handling the errors and completion
    173. Errors & Completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { interval, Subscription } from 'rxjs';
 import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
@@ -16,9 +15,29 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2){
+          observer.complete();
+        }
+        if (count > 3){
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
     });
+
+    this.firstObsSubscription = customIntervalObservable
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        console.log(error);
+        alert(error);
+      }, () => {
+        console.log('Completed!');
+      });
   }
 
   ngOnDestroy(): void {
