5e66380ab4a769a67c17d1c870012bc8f9223ad2
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-10-18 18:07:56 +0300

    Another test project to learn observable; Unsubscribe;
    169. Module Introduction
    170. Analyzing Angular Observables
    171. Getting Closer to the Core of Observables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
-import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
-const appRoutes: Routes = [
-    {path: '', redirectTo: 'recipes', pathMatch: 'full'},
-    {path: 'recipes', component: RecipesComponent},
-    {path: 'shopping-list', component: ShoppingListComponent}
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'user/:id', component: UserComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule{
+export class AppRoutingModule {
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
-import { DropdownDirective } from './shared/dropdown.directive';
-
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import { ShoppingService } from './shopping-list/shopping.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeListComponent,
-    ShoppingListComponent,
-    DropdownDirective,
-    ShoppingEditComponent
-   ],
+    HomeComponent,
+    UserComponent
+  ],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule
   ],
-  providers: [ShoppingService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
