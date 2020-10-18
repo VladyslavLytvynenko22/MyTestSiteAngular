8efaaee6fb2fa639c55fa28493a9338308c143fa
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-10-18 20:52:07 +0300

    Using subjects
    176. Subjects
    177. Wrap Up
    178. Useful Resources & Links
    
    Useful Resources:
    
    Official Docs: https://rxjs-dev.firebaseapp.com/
    
    RxJS Series: https://academind.com/learn/javascript/understanding-rxjs/
    
    Updating to RxJS 6: https://academind.com/learn/javascript/rxjs-6-what-changed/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  activated = false;
+  private activatedSub: Subscription;
 
-  ngOnInit(): void{}
+  constructor(private userSrv: UserService) {}
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
+
+  ngOnInit(): void{
+    this.activatedSub = this.userSrv.activatedEmmiter.subscribe((activated: boolean) => {
+      this.activated = activated;
+    });
+  }
 }
