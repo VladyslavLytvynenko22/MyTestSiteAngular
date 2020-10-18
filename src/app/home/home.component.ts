45a639174cdaab01b08e24dc52d463b5836ab6ca
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-10-18 19:02:54 +0300

    Creating my own observable
    172. Building a Custom Observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
