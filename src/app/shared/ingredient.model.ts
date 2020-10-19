9f96506da0d30871d1eb5c3769962654046a45b7
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-10-19 23:06:52 +0300

    initial commit before new test project

--- src/app/shared/ingredient.model.ts
+++
@@ -1,9 +0,0 @@
-export class Ingredient {
-    public name: string;
-    public amount: number;
-
-    constructor(ingredientName: string, amount: number){
-        this.name = ingredientName;
-        this.amount = amount;
-    }
-}
