cd0be2ecb11950f73e4568431df4b06950eb9668
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-10-21 20:26:38 +0300

    use default values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
 
   suggestUserName(): void {
     const suggestedName = 'Superuser';
