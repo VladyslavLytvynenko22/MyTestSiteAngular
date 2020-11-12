53948af5993e1268b53384b57215e4f369ad0fa2
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-11-12 23:54:59 +0200

    Pipe
    239. Introduction & Why Pipes are Useful
    240. Using Pipes
    241. Parametrizing Pipes
    242. Where to learn more about Pipes
    243. Chaining Multiple Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
-   ],
+  ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
