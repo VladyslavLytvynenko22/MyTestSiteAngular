48dfc62425869f9339f8981160964603a5362535
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-11-15 23:04:42 +0200

    Showing a Loading Indicator
    259. Showing a Loading Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -38,6 +39,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPost(): void {
+    this.isFetching = true;
     this.http.get<{ [key: string]: Post}>('https://mytest1-320c9.firebaseio.com/posts.json')
       .pipe(
         map(responseData => {
@@ -51,6 +53,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(post => {
+        this.isFetching = false;
         this.loadedPosts = post;
       });
   }
