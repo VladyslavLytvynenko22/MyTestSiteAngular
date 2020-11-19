9e5f1ffb9a70b338e6c3a93d06cb7eb9604c8c9c
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-11-19 21:39:00 +0200

    Interceptors Manipulating
    272. Manipulating Request Objects
    273. Response Interceptors

--- src/app/auth-intercetor.service.ts
+++ src/app/auth-intercetor.service.ts
@@ -1,9 +1,19 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
         console.log('Request is on its way');
-        return next.handle(req);
+        console.log(req.url);
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth', 'xyz')
+        });
+        return next.handle(modifiedRequest).pipe(tap(event => {
+            if (event.type === HttpEventType.Response) {
+                console.log('Response arrived, body data: ');
+                console.log(event.body);
+            }
+        }));
     }
 }
