ea72ec467c19fed59a237db1821ebbc3a7ea6484
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-12-06 00:34:32 +0200

    Deploying an Angular App to FIrebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyDpTki8u4d9fMCOJHzJMvxS9arDeWMCZlM'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyDpTki8u4d9fMCOJHzJMvxS9arDeWMCZlM'
 };
 
 /*
