a76a3292190c4fe384c5886dd7af3511e21c3586
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-12-05 02:29:57 +0200

    Auth Module
    328. Adding an Auth Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,28 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
+
+import { AuthComponent } from './auth.component';
+
+@NgModule({
+    declarations: [
+        AuthComponent
+    ],
+    imports: [
+        FormsModule,
+        SharedModule,
+        AuthRoutingModule
+    ]
+})
+export class AuthModule {
+
+}
