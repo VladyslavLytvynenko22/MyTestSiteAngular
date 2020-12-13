d139ac78cda69b1f53b48cbdc01b0cfd84edf83c
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-12-13 23:43:36 +0200

    Setting Up Auth Reducer
    360. Setting Up Auth Reducer & Actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,3 +1,5 @@
+import * as AuthActions from './auth.actions';
+
 import { User } from './../user.model';
 
 export interface State {
@@ -8,6 +10,21 @@ const initialState: State = {
     user: null
 };
 
-export function authReducer(state = initialState, action): any {
-    return state;
+export function authReducer(state = initialState,
+                            action: AuthActions.AuthActions): any {
+    switch (action.type) {
+        case AuthActions.LOGIN:
+            const user = action.payload;
+            return {
+                ...state,
+                user
+            };
+        case AuthActions.LOGOUT:
+            return {
+                ...state,
+                user: null
+            };
+        default:
+            return state;
+    }
 }
