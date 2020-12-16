683407c117354d72d3bacdf3e3d00a5fc50bf8c6
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2020-12-16 22:06:05 +0200

    Revert "updating"
    This reverts commit a7b0429ae7ca00afaabb97b52b32d5f07d64f4cf.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })],
+    imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
